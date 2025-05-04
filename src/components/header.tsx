bfc8c38e6c95cdd315c9e27c33d388f6286effd3
Pedro Moraes <pmoraes@esapiens.com.br>
2025-05-04 20:09:20 -0300

    chore: initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'waku'
+
+export const Header = () => {
+  return (
+    <header className="flex items-center gap-4 p-6 lg:fixed lg:top-0 lg:left-0">
+      <h2 className="font-bold text-lg tracking-tight">
+        <Link to="/">Produtos</Link>
+      </h2>
+      <h2 className="font-bold text-lg tracking-tight">
+        <Link to="/about">Sobre a empresa</Link>
+      </h2>
+      <h2 className="font-bold text-lg tracking-tight">
+        <Link to="/users-feedback">Avaliações dos usuários</Link>
+      </h2>
+    </header>
+  )
+}
---
+++ src/pages/users-feedback.tsx
@@ -0,0 +1,9 @@
+const UsersFeedback = () => {
+  return (
+    <div>
+      <h1>Users Feedback</h1>
+      <p>This is the users feedback page.</p>
+    </div>
+  )
+}
+export default UsersFeedback
