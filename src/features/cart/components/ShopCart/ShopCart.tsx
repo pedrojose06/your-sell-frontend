db53d9e39533640ae3b6ae8e871121fefdfce3fd
Pedro Moraes <pmoraes@esapiens.com.br>
2025-05-08 22:13:44 -0300

    fix: remove mock products from shop-cart

--- src/features/cart/components/ShopCart/ShopCart.tsx
+++ src/features/cart/components/ShopCart/ShopCart.tsx
@@ -8,34 +8,6 @@ import {
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { useShopCartProducts } from '../../hooks/useShopCartProducts'
 
-const products = [
-  {
-    id: 1,
-    name: 'Throwback Hip Bag',
-    href: '#',
-    color: 'Salmon',
-    price: '$90.00',
-    quantity: 1,
-    imageSrc:
-      'https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-    imageAlt:
-      'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-  },
-  {
-    id: 2,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    color: 'Blue',
-    price: '$32.00',
-    quantity: 1,
-    imageSrc:
-      'https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-    imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-  },
-  // More products...
-]
-
 interface IShoppingCart {
   open: boolean
   setOpen: (open: boolean) => void
