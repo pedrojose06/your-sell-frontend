612bb7adab4c811852deaaee6cc4a63e6452683a
Pedro Moraes <pmoraes@esapiens.com.br>
2025-05-09 19:23:17 -0300

    feat: add badge to shopCart and put header like stiky top

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { useAtomValue } from 'jotai'
 import { lazy, useState } from 'react'
 import { IoCartOutline } from 'react-icons/io5'
 import { Link } from 'waku'
+import { atomShopCartProducts } from '../features/cart/atoms/shop-cart'
 
 const ShoppingCart = lazy(
   () => import('../features/cart/components/ShopCart/ShopCart')
@@ -10,9 +12,13 @@ const ShoppingCart = lazy(
 
 export const Header = () => {
   const [openCart, setOpenCart] = useState(false)
+  const cartQuantity = useAtomValue(atomShopCartProducts)
+  const badgeQuantity = cartQuantity.reduce((acc, product) => {
+    return acc + product.quantity
+  }, 0)
 
   return (
-    <header className="z-20 flex w-full items-center justify-between gap-4 bg-white p-6 shadow-md lg:fixed lg:top-0 lg:left-0">
+    <header className="sticky top-0 z-20 flex w-full items-center justify-between gap-4 bg-white p-6 shadow-md lg:fixed lg:top-0 lg:left-0">
       <Link to="/products">
         <img src="src/assets/pao.png" alt="Logo" className="h-12 w-14" />
       </Link>
@@ -27,10 +33,15 @@ export const Header = () => {
           <Link to="/users-feedback">Avaliações dos usuários</Link>
         </h2>
       </div>
-      <IoCartOutline
-        className="h-8 w-8 cursor-pointer text-gray-700"
-        onClick={() => setOpenCart(true)}
-      />
+      <div className="relative">
+        <IoCartOutline
+          className="h-8 w-8 cursor-pointer text-gray-700"
+          onClick={() => setOpenCart(true)}
+        />
+        <span className="-top-2 -right-2 absolute flex h-5 w-5 items-center justify-center rounded-full bg-red-500 font-bold text-white text-xs">
+          {badgeQuantity}
+        </span>
+      </div>
       {openCart && <ShoppingCart open={openCart} setOpen={setOpenCart} />}
     </header>
   )
